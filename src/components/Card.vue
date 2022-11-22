5a401f71593193ac575ec7f57367c254087d813b
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2022-11-22 17:37:55 +0100

    [good-hover-effect]work-on-back-info

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -68,8 +68,9 @@ export default {
 </script>
 <template>
   <div class="dc-card card" v-show="elem.poster_path">
-    <img :src="`${store.imgUrl}${elem.poster_path}`" class="card-img-top" :alt="elem.title">
-    <div class="card-body">
+    <img :src="`${store.imgUrl}${elem.poster_path}`" class="dc-card-img dc-card-front" :alt="elem.title">
+    <div class="dc-card-overlay"></div>
+    <div class="card-body dc-card-back d-none">
       <h4 class="card-text">{{elem.title||elem.name}}</h4>
       <p class="card-sum">{{elem.overview}}</p>
       <p class="card-text">Lingua originale:
@@ -90,8 +91,62 @@ export default {
 
 <style lang="scss" scoped>
   @use '../styles/general';
+  @use '../styles/partials/variables' as *;
+
   .dc-card[data-v-c6c3362a]{
-    width: calc(100% / 5)
+    width: calc(100% / 5);
+    max-height:400px;
+    aspect-ratio: 4 / 5;
+    position:relative;
+    padding:0;
+    .dc-card-img{
+      height:100%;
+      width:100%;
+      object-fit: cover;
+    }
+  }
+  .dc-card-overlay{
+    opacity: 0;
+    border:1px solid $primary-red;
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    background-color: rgba(0,0,0,0.8);
+    color:white;
+    border-radius: 50%;
+    box-shadow: 0 2px 4px rgba(#000000, 0.2);
+    transition: 0.5s;
+    cursor: pointer;
+
+    &::before {
+      content: "\f129";
+      font-size: 16px;
+      font-family: "Font Awesome 5 Free"; 
+      font-weight: 900;
+    }
+    &:hover,
+    &:focus {
+      bottom: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      box-shadow: none;
+      border-radius: 0;
+      opacity: 0.95;
+      border:none;
+    
+    &::before {
+      content: none;
+    }
+  }
+  .dc-card-img:hover ~ &{
+    opacity:1;
+  }
   }
   .mini-flag{
     max-height:15px;
